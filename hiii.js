f559a439d4142c46b25da759a070eeff89c130ff
Rupa Reddymasu <rupareddymasu@gmail.com>
2024-02-05 08:18:36 +0530

    first commit

---
+++ hiii.js
@@ -0,0 +1,64 @@
+// var a=1;
+// var b=0;
+// var c=1;
+// var d=a+b+c;
+// console.log(d);
+
+// var a=+prompt("enter a value");
+// var b=+prompt("enter a value");
+// var c=+prompt("enter a value");
+// function add(a,b,c){
+//     return a+b+c;
+// }
+// console.log(add(12,12,12));
+
+// let add=(a,b,c) =>
+// a+b+c;
+// console.log(add(12,12,12,));
+
+// let number=7869;
+function reverse(name){
+    let a=name;
+    // let a=number.toString();
+    let b=a.split("").reverse().join("");
+    // let c=parseInt(b);
+
+    return b;
+}
+d=reverse("Rupa")
+console.log(d)
+
+var ah=["abc","bcb","lkm","jnm"];
+var as=ah.reverse();
+console.log(as);
+
+
+// let arr=[5,4,3,2,1];
+// let reverse1=[];
+// for(let i=arr.length-1;i>=0;i--){
+//     debugger;
+//     const o=reverse1[arr[i]]
+//     // let ju=rupa[i];
+//    reverse1.push(o);
+
+//    console.log(reverse1.push(o));
+
+// }
+
+
+let arr=[5,4,3,2,1];
+let reverse1=[];
+for(let i=arr.length-1;i>=0;i--){
+    debugger;
+    reverse1.push(arr[i]);
+    console.log(reverse1)
+
+}
+// console.log(ak);
+// console.log(reverse1.push(o));
+
+
+
+
+
+
